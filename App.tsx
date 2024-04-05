ce30869165981bc457fa3d68729943b5221b60cf
justinmihigo <mihigojustin28@gmail.com>
2024-04-05 14:22:57 +0200

    Initial commit
    Initial commit with project template

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import SplashPage from './pages/SplashPage';
+import React from 'react';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SplashPage/>
+      <Text>Welcom</Text>
     </View>
   );
 }
