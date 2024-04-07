02552733de0d7c4d8b01d052e8ae6f480222167b
justinmihigo <mihigojustin28@gmail.com>
2024-04-07 15:00:52 +0200

    feat: welcome screen and configure splash screen
    create welcome screen
    add animation on the images using flatlist
    configure the image for splash screen in app.json
    
    pivotal tracker ID #187382657

---
+++ components/buttonwhite.tsx
@@ -0,0 +1,40 @@
+import { View,Pressable, Text, StyleSheet } from "react-native";
+import {useFonts} from "expo-font"
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome"
+import {faPhone} from "@fortawesome/free-solid-svg-icons"
+const ButtonWhite: React.FC=()=>{
+    const [fontsLoaded]= useFonts({'Avenir': require('../assets/avenir_ff/AvenirLTStd-Book.otf')})
+    if(!fontsLoaded) return null;
+    return(
+        <View>
+            <Pressable style={styles.button} onPress={()=>alert("Hello")}>
+                <View style={styles.flex}>
+                    <FontAwesomeIcon icon={faPhone} size={20} />
+                    <Text style={styles.btntxt}> Login with phone</Text>
+                </View>
+            </Pressable>
+            
+        </View>
+    )
+}
+export default ButtonWhite;
+const styles = StyleSheet.create({
+     button:{
+         backgroundColor: 'white',
+         width: '100%',
+         height: 60,
+         borderRadius: 10,
+         justifyContent: 'center',
+         alignItems: 'center',
+         marginTop: 10,
+         marginBottom: 20,
+     },
+     btntxt:{
+         fontSize: 17,
+         fontFamily:'Avenir',
+     },
+     flex:{
+        flexDirection: 'row',
+        gap: 10,
+     }
+})
